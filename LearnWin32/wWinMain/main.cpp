da5885a375c023dc25fa44b0a59b16df8b36fc10
mint233 <daizicun@163.com>
2026-07-31 05:36:32 +0800

    2026/7/31 5:35 新增鼠标左键画线功能

--- LearnWin32/wWinMain/main.cpp
+++ LearnWin32/wWinMain/main.cpp
@@ -4,10 +4,59 @@
 
 #include <windows.h>
 #include <windowsx.h>
+#include <d2d1.h>
+#pragma comment(lib, "d2d1")
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-int xPos, yPos; // 记录鼠标坐标
+int xPos, yPos;     // 记录鼠标坐标
+int xPoint0, yPoint0, xPoint1, yPoint1; // 绘制起点与终点
+bool IsPointing = false;    // 是否处于绘制状态
+
+ID2D1Factory*          pFactory;
+ID2D1HwndRenderTarget* pRenderTarget;
+ID2D1SolidColorBrush*  pBrush;
+
+// 创建 D2D 资源
+HRESULT CreateGraphicsResources(HWND hwnd)
+{
+    HRESULT hr = S_OK;
+    if (pRenderTarget == NULL)
+    {
+        RECT rc;
+        GetClientRect(hwnd, &rc);
+
+        D2D1_SIZE_U size = D2D1::SizeU(rc.right, rc.bottom);
+
+        hr = pFactory->CreateHwndRenderTarget(
+            D2D1::RenderTargetProperties(),
+            D2D1::HwndRenderTargetProperties(hwnd, size),
+            &pRenderTarget);
+
+        if (SUCCEEDED(hr))
+        {
+            const D2D1_COLOR_F color = D2D1::ColorF(0,0,256);
+            hr = pRenderTarget->CreateSolidColorBrush(color, &pBrush);
+        }
+    }
+    return hr;
+}
+
+template <class T> void SafeRelease(T** ppT)
+{
+    if (*ppT)
+    {
+        (*ppT)->Release();
+        *ppT = NULL;
+    }
+}
+// 释放 D2D 资源
+void DiscardGraphicsResources()
+{
+    SafeRelease(&pRenderTarget);
+    SafeRelease(&pBrush);
+}
+
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
 {
@@ -65,12 +114,25 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     switch (uMsg)
     {
     case WM_DESTROY:
+    {
         PostQuitMessage(0);
         return 0;
+    }
+
+    // 启动时创建pFactory
+    case WM_CREATE:
+    {
+        if (FAILED(D2D1CreateFactory(
+            D2D1_FACTORY_TYPE_SINGLE_THREADED, &pFactory)))
+        {
+            return -1;  // Fail CreateWindowEx.
+        }
+        return 0;
+    }
 
     case WM_PAINT:
     {
-        PAINTSTRUCT ps;
+        /*PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hwnd, &ps);
 
         RECT rect;
@@ -80,51 +142,116 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         FillRect(hdc, &rect, (HBRUSH)(COLOR_WINDOW + 1));
 
         // 格式化数字
-        wchar_t buffer[64];
-        wsprintf(buffer, L"MOUSE: (%d, %d)", xPos, yPos);
+        // wchar_t buffer[64];
+        // wsprintf(buffer, L"MOUSE: (%d, %d)", xPos, yPos);
 
         // 设置文字颜色
         SetTextColor(hdc, RGB(0, 0, 255));  // 蓝色
         SetBkMode(hdc, TRANSPARENT);        // 透明背景
 
         // 在窗口中央绘制
-        DrawText(hdc, buffer, -1, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+        // DrawText(hdc, buffer, -1, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
 
         // All painting occurs here, between BeginPaint and EndPaint.
         // FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-        EndPaint(hwnd, &ps);
+        EndPaint(hwnd, &ps);*/
+
+        HRESULT hr = CreateGraphicsResources(hwnd);
+        if (SUCCEEDED(hr))
+        {
+            PAINTSTRUCT ps;
+            BeginPaint(hwnd, &ps);
+
+            pRenderTarget->BeginDraw();
+
+
+            /*// 清屏（白色背景）
+            pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::White));
+
+            // 画一条蓝色线，从 (50, 50) 到 (300, 200)
+            pRenderTarget->DrawLine(
+                D2D1::Point2F(50.0f, 50.0f),
+                D2D1::Point2F(300.0f, 200.0f),
+                pBrush,
+                3.0f  // 线宽
+            );*/
+
+            pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::White));
+
+            pRenderTarget->DrawLine(
+                D2D1::Point2F(xPoint0, yPoint0),
+                D2D1::Point2F(xPoint1, yPoint1),
+                pBrush,
+                3.0f  // 线宽
+            );
+
+
+
+            hr = pRenderTarget->EndDraw();
+            if (FAILED(hr) || hr == D2DERR_RECREATE_TARGET)
+            {
+                DiscardGraphicsResources();
+            }
+            EndPaint(hwnd, &ps);
+        }
+
         return 0;
     }
 
     case WM_LBUTTONDOWN:
     {
-        xPos = GET_X_LPARAM(lParam);
+        /*xPos = GET_X_LPARAM(lParam);
         yPos = GET_Y_LPARAM(lParam);
-        InvalidateRect(hwnd, NULL, TRUE);  // 触发重绘
+        InvalidateRect(hwnd, NULL, TRUE);*/  // 触发重绘
+
+        xPoint0 = GET_X_LPARAM(lParam);
+        yPoint0 = GET_Y_LPARAM(lParam);
+        IsPointing = true;
+
         return 0;
     }
 
     case WM_LBUTTONUP:
     {
-        xPos = GET_X_LPARAM(lParam);
+        /*xPos = GET_X_LPARAM(lParam);
         yPos = GET_Y_LPARAM(lParam);
-        InvalidateRect(hwnd, NULL, TRUE);  // 触发重绘
+        InvalidateRect(hwnd, NULL, TRUE);*/  // 触发重绘
+
+        IsPointing = false;
+
         return 0;
     }
 
     case WM_MOUSEMOVE: 
     {
-        xPos = GET_X_LPARAM(lParam);
+        /*xPos = GET_X_LPARAM(lParam);
         yPos = GET_Y_LPARAM(lParam);
-        InvalidateRect(hwnd, NULL, TRUE);  // 触发重绘
+        InvalidateRect(hwnd, NULL, TRUE);*/  // 触发重绘
+
+        if (IsPointing) {
+            xPoint1 = GET_X_LPARAM(lParam);
+            yPoint1 = GET_Y_LPARAM(lParam);
+            InvalidateRect(hwnd, NULL, FALSE);
+        }
+
         return 0;
     }
 
     case WM_SIZE:
     {
-        // 窗口大小改变时，强制重绘整个窗口
-        InvalidateRect(hwnd, NULL, TRUE);
-        return 0;
+        /*// 窗口大小改变时，强制重绘整个窗口
+        InvalidateRect(hwnd, NULL, TRUE);*/
+
+        if (pRenderTarget != NULL)
+        {
+            RECT rc;
+            GetClientRect(hwnd, &rc);
+
+            D2D1_SIZE_U size = D2D1::SizeU(rc.right, rc.bottom);
+
+            pRenderTarget->Resize(size);
+            InvalidateRect(hwnd, NULL, FALSE);
+        }
     }
 
     return 0;

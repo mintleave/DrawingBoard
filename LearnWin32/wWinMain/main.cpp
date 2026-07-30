96c503ee2fc01dea2dc5cd4ee16b501a3ae2bb81
mint233 <daizicun@163.com>
2026-07-31 03:22:46 +0800

    2026/7/31 3:21 新增打印鼠标坐标的功能

--- LearnWin32/wWinMain/main.cpp
+++ LearnWin32/wWinMain/main.cpp
@@ -3,8 +3,11 @@
 #endif 
 
 #include <windows.h>
+#include <windowsx.h>
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int xPos, yPos; // 记录鼠标坐标
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
 {
@@ -17,6 +20,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
     wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInstance;
     wc.lpszClassName = CLASS_NAME;
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);  // 白色背景
 
     RegisterClass(&wc);
 
@@ -68,10 +72,60 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hwnd, &ps);
 
+        RECT rect;
+        GetClientRect(hwnd, &rect);
+
+        // 用背景色填充整个客户区（防止残留）
+        FillRect(hdc, &rect, (HBRUSH)(COLOR_WINDOW + 1));
+
+        // 格式化数字
+        wchar_t buffer[64];
+        wsprintf(buffer, L"MOUSE: (%d, %d)", xPos, yPos);
+
+        // 设置文字颜色
+        SetTextColor(hdc, RGB(0, 0, 255));  // 蓝色
+        SetBkMode(hdc, TRANSPARENT);        // 透明背景
+
+        // 在窗口中央绘制
+        DrawText(hdc, buffer, -1, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+
         // All painting occurs here, between BeginPaint and EndPaint.
-        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+        // FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
         EndPaint(hwnd, &ps);
+        return 0;
     }
+
+    case WM_LBUTTONDOWN:
+    {
+        xPos = GET_X_LPARAM(lParam);
+        yPos = GET_Y_LPARAM(lParam);
+        InvalidateRect(hwnd, NULL, TRUE);  // 触发重绘
+        return 0;
+    }
+
+    case WM_LBUTTONUP:
+    {
+        xPos = GET_X_LPARAM(lParam);
+        yPos = GET_Y_LPARAM(lParam);
+        InvalidateRect(hwnd, NULL, TRUE);  // 触发重绘
+        return 0;
+    }
+
+    case WM_MOUSEMOVE: 
+    {
+        xPos = GET_X_LPARAM(lParam);
+        yPos = GET_Y_LPARAM(lParam);
+        InvalidateRect(hwnd, NULL, TRUE);  // 触发重绘
+        return 0;
+    }
+
+    case WM_SIZE:
+    {
+        // 窗口大小改变时，强制重绘整个窗口
+        InvalidateRect(hwnd, NULL, TRUE);
+        return 0;
+    }
+
     return 0;
     }
 

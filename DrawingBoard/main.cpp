630ec527bc35629c630cd48c7bb3f5dfa0b71d6e
mint233 <daizicun@163.com>
2026-07-31 15:38:54 +0800

    2026/7/31 15:38 创建main.cpp, 定义程序入口

---
+++ DrawingBoard/main.cpp
@@ -0,0 +1,11 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#include <windows.h>
+
+// win32程序的入口
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
+{
+	return 0;
+}
